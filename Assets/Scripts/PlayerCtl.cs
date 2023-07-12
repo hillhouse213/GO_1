e5aa886155d1704940230c15464947a7d986c0b7
KIMWOOBIN <woodol4540@naver.com>
2023-07-12 09:08:38 +0900

    Day0712

--- Assets/Scripts/PlayerCtl.cs
+++ Assets/Scripts/PlayerCtl.cs
@@ -38,6 +38,8 @@ public class PlayerCtl : MonoBehaviour
     [SerializeField]
     private Camera theCamera;
 
+    private Animator ani;
+
     void Start()
     {
         myRigid = GetComponent<Rigidbody>();
@@ -45,6 +47,7 @@ public class PlayerCtl : MonoBehaviour
         capsuleCollider = GetComponent<CapsuleCollider>();
         originPosY = theCamera.transform.localPosition.y;
         applyCrouchPosY = originPosY;
+        ani = GetComponent<Animator>();
     }
     void Update()
     {
@@ -68,6 +71,10 @@ public class PlayerCtl : MonoBehaviour
         Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
 
         myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+        ani.SetBool("Walk", _velocity.magnitude > 0);
+        ani.SetBool("Run", isRun);
+        Debug.Log(ani.GetBool("Walk") + "°È±â");
+        Debug.Log(ani.GetBool("Run") + "¶Ù±â");
     }
     //Ä«¸Þ¶ó »óÇÏ
     private void CameraRotation()
